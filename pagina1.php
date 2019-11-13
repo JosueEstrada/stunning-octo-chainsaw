229d276f2b987a2c24179ed06f16ca9dcad2da85
Randolph <Randolph@192.168.1.61>
2019-11-12 23:32:36 -0500

    Agregado pagina1, pagina2, ejm Hipervinculos

---
+++ pagina1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Pagina1</title>
+    </head>
+    <body>
+        <h1>Página principal.</h1>
+        <a href="pagina2.php">Noticias</a>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ pagina2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Página 2</title>
+    </head>
+    <body>
+        <h1>Noticias.</h1>
+        <a href="pagina1.php">Salir.</a>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
